f0ef46a1da3a02f1792a8cd56e6b2a67c175de84
nikhil <contact.nikhilnambiar@gmail.com>
2021-09-01 23:06:55 +0530

    first

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, redirect, url_for, flash,abort,request
 from flask_ckeditor import CKEditor
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 from db import *
+from flask_oauth import OAuth
 
 
 app=Flask(__name__)
@@ -9,7 +10,7 @@ app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
-
+oauth=OAuth(app)
 
 ckeditor=CKEditor(app)
 
