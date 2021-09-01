3e2fbbebada695a4cd994d96addab768194d3a96
nikhil <contact.nikhilnambiar@gmail.com>
2021-09-01 22:09:17 +0530

    first

---
+++ db.py
@@ -0,0 +1,58 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship
+from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
+db=SQLAlchemy()
+
+class User(UserMixin,db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    posts = relationship("BlogPost", back_populates="author")
+    issueposts=relationship("IssueBlogPost", back_populates="author")
+    comments = relationship("Comment", back_populates="comment_author")
+    issuecomments=relationship("IssueComment", back_populates="comment_author")
+
+class BlogPost(db.Model):
+    __tablename__ = "blog_posts"
+    id = db.Column(db.Integer, primary_key=True)
+    author_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    author = relationship("User", back_populates="posts")
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    subtitle = db.Column(db.String(250), nullable=False)
+    date = db.Column(db.String(250), nullable=False)
+    body = db.Column(db.Text, nullable=False)
+    img=db.Column(db.Text)
+    filetype = db.Column(db.Text, nullable=False)
+    comments = relationship("Comment", back_populates="parent_post")
+
+
+class Comment(db.Model):
+    __tablename__ = "comments"
+    id = db.Column(db.Integer, primary_key=True)
+    post_id = db.Column(db.Integer, db.ForeignKey("blog_posts.id"))
+    author_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    parent_post = relationship("BlogPost", back_populates="comments")
+    comment_author = relationship("User", back_populates="comments")
+    text = db.Column(db.Text, nullable=False)
+
+
+class IssueBlogPost(db.Model):
+    __tablename__ = "issueblog_posts"
+    id = db.Column(db.Integer, primary_key=True)
+    author_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    author = relationship("User", back_populates="issueposts")
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    date = db.Column(db.String(250), nullable=False)
+    body = db.Column(db.Text, nullable=False)
+    img=db.Column(db.Text)
+    comments = relationship("IssueComment", back_populates="parent_post")
+class IssueComment(db.Model):
+    __tablename__ = "issuecomments"
+    id = db.Column(db.Integer, primary_key=True)
+    post_id = db.Column(db.Integer, db.ForeignKey("issueblog_posts.id"))
+    author_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    parent_post = relationship("IssueBlogPost", back_populates="comments")
+    comment_author = relationship("User", back_populates="issuecomments")
+    text = db.Column(db.Text, nullable=False)
--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 from flask import Flask, render_template, redirect, url_for, flash,abort,request
-
+from flask_ckeditor import CKEditor
+from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
+from db import *
 
 app=Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+
+ckeditor=CKEditor(app)
+
+
+# login_manager=LoginManager()
+# login_manager.init_app(app)
+#
+# @login_manager.user_loader()
+# def load_user(user_id):
+#     return User.query.get(str(user_id))
 
 @app.route('/',methods=["GET","POST"])
 def start():
