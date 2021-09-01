55e64f005c374d016b59896d954b9e772a55d80b
nikhil <contact.nikhilnambiar@gmail.com>
2021-09-01 23:11:58 +0530

    first

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
-oauth=OAuth()
 
 ckeditor=CKEditor(app)
 
