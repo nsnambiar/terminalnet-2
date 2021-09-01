5d28713ad546e377c7ec42b33d063231f3b2a387
nikhil <contact.nikhilnambiar@gmail.com>
2021-09-01 23:15:06 +0530

    first

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ from flask import Flask, render_template, redirect, url_for, flash,abort,request
 from flask_ckeditor import CKEditor
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 from db import *
-from flask_oauth import OAuth
-
+from flask_oauthlib.client import OAuth
 
 app=Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
