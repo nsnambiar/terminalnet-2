e2d0f3feedc0dbe33b2ca0ce855b994d07882a46
nikhil <contact.nikhilnambiar@gmail.com>
2021-09-01 23:02:51 +0530

    first

--- main.py
+++ main.py
@@ -13,21 +13,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
 
-google = oauth.register(
-    name = 'google',
-    client_id = "175887749203-gdtkc1h93svnal7gspl415t24ggfd5be.apps.googleusercontent.com",
-    client_secret = "90KgzC26EfXluVqju8LJkRrA",
-    access_token_url = 'https://accounts.google.com/o/oauth2/token',
-    access_token_params = None,
-    authorize_url = 'https://accounts.google.com/o/oauth2/auth',
-    authorize_params = None,
-    api_base_url = 'https://www.googleapis.com/oauth2/v1/',
-    userinfo_endpoint = 'https://openidconnect.googleapis.com/v1/userinfo',  # This is only needed if using openId to fetch user info
-    client_kwargs = {'scope': 'openid email profile'},
-)
-
-
-
 ckeditor=CKEditor(app)
 
 
