aafb0d95bae915882cd8d885f16e5466bbad74bf
nikhil <contact.nikhilnambiar@gmail.com>
2021-09-01 23:26:23 +0530

    first

--- main.py
+++ main.py
@@ -13,23 +13,6 @@ db.init_app(app)
 ckeditor=CKEditor(app)
 
 
-oauth=OAuth()
-import os
-
-
-google = oauth.remote_app(
-    name = 'google',
-    client_id = os.environ['GOOGLE_CLIENT_ID'],
-    client_secret = os.environ['GOOGLE_CLIENT_SECRET'],
-    access_token_url = 'https://accounts.google.com/o/oauth2/token',
-    access_token_params = None,
-    authorize_url = 'https://accounts.google.com/o/oauth2/auth',
-    authorize_params = None,
-    api_base_url = 'https://www.googleapis.com/oauth2/v1/',
-    userinfo_endpoint = 'https://openidconnect.googleapis.com/v1/userinfo',  # This is only needed if using openId to fetch user info
-    client_kwargs = {'scope': 'openid email profile'},
-)
-
 
 # login_manager=LoginManager()
 # login_manager.init_app(app)
