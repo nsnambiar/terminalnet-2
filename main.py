1902370db419da0a3389931d7d4b7b95a75cbd36
nikhil <contact.nikhilnambiar@gmail.com>
2021-09-02 06:53:05 +0530

    first

--- main.py
+++ main.py
@@ -13,6 +13,19 @@ db.init_app(app)
 ckeditor=CKEditor(app)
 
 oauths = OAuth(app)
+google = oauths.register(
+    name = 'google',
+    client_id = "175887749203-gdtkc1h93svnal7gspl415t24ggfd5be.apps.googleusercontent.com",
+    client_secret = "90KgzC26EfXluVqju8LJkRrA",
+    access_token_url = 'https://accounts.google.com/o/oauth2/token',
+    access_token_params = None,
+    authorize_url = 'https://accounts.google.com/o/oauth2/auth',
+    authorize_params = None,
+    api_base_url = 'https://www.googleapis.com/oauth2/v1/',
+    userinfo_endpoint = 'https://openidconnect.googleapis.com/v1/userinfo',  # This is only needed if using openId to fetch user info
+    client_kwargs = {'scope': 'openid email profile'},
+)
+
 
 # login_manager=LoginManager()
 # login_manager.init_app(app)
