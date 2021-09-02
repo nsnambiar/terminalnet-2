91074a093f2b28ec814fc2811affb565a71ba6a5
nikhil <contact.nikhilnambiar@gmail.com>
2021-09-02 10:02:05 +0530

    initial

--- main.py
+++ main.py
@@ -34,9 +34,9 @@ google = oauths.register(
 # login_manager=LoginManager()
 # login_manager.init_app(app)
 
-@login_manager.user_loader()
-def load_user(user_id):
-    return User.query.get(str(user_id))
+# @login_manager.user_loader()
+# def load_user(user_id):
+#     return User.query.get(str(user_id))
 
 @app.route('/',methods=["GET","POST"])
 def start():
