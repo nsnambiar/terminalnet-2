dbbdcd8c9dfdf003d5ca0a55642c87ee5a457527
nikhil <contact.nikhilnambiar@gmail.com>
2021-09-05 21:11:39 +0530

    initial

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ import os
 
 app=Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
