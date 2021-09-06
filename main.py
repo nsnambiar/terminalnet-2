bb54faa0e14ba1d9ad3acd4a0a0895ce79a66479
nikhil <contact.nikhilnambiar@gmail.com>
2021-09-06 23:44:30 +0530

    initial

--- main.py
+++ main.py
@@ -52,16 +52,21 @@ def load_user(user_id):
 @app.route('/',methods=["GET","POST"])
 def start():
     db.create_all()
-    return redirect(url_for('view',page=0))
-
-
-@app.route('/Page:<int:page>',methods=['GET'])
-def view(page):
+    page = request.args.get('Page', 1, type=int)
     per_page = 5
-    posts = BlogPost.query.order_by(BlogPost.date.desc()).paginate(page,per_page,error_out=False)
+    posts = BlogPost.query.order_by(BlogPost.date.desc()).paginate(page, per_page, error_out=False)
     issues = IssueBlogPost.query.order_by(desc(IssueBlogPost.id)).all()
     return render_template("index.html", all_post=posts, currentuser=current_user, issue_post=issues)
 
+
+# @app.route('/Page',methods=['GET'])
+# def view():
+#     page = request.args.get('Page', 1, type=int)
+#     per_page = 5
+#     posts = BlogPost.query.order_by(BlogPost.date.desc()).paginate(page,per_page,error_out=False)
+#     issues = IssueBlogPost.query.order_by(desc(IssueBlogPost.id)).all()
+#     return render_template("index.html", all_post=posts, currentuser=current_user, issue_post=issues)
+#
 
 
 
