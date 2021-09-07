e9be2a852766d98d89d5c3bd9e0d65911fe2fd45
nikhil <contact.nikhilnambiar@gmail.com>
2021-09-07 20:01:50 +0530

    initial

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ def load_user(user_id):
 @app.route('/',methods=["GET","POST"])
 def start():
     db.create_all()
-    posts = BlogPost.query.order_by(BlogPost.date.desc()).all()
+    posts = BlogPost.query.all()
     issues = IssueBlogPost.query.order_by(desc(IssueBlogPost.id)).all()
     return render_template("index.html", all_post=posts, currentuser=current_user, issue_post=issues)
 
